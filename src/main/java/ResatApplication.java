aab95f07b74660be153f78ac3c52022ba705f924
AlbertoMrMekko <albertomartinsanz1@gmail.com>
2025-01-12 12:15:43 +0100

    first working setup

---
+++ src/main/java/ResatApplication.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ResatApplication extends Application
+{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        // todo replace example code
+        primaryStage.setTitle("Mi Aplicación JavaFX");
+        primaryStage.setWidth(800);
+        primaryStage.setHeight(600);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
